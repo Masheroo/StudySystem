99e35899b5d90ad4c565b214115fc16ada5bef8b
Mashero <dima.vuynov@yandex.ru>
2022-10-12 13:21:36 +0300

    Начало главной страницы

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title>Главная</title>
+</head>
+<body>
+
+    <section class="header">
+
+        <div class="header-nav">
+            
+            <div class="logo">
+                <h1 class="">LOGO</h1>
+            </div>
+
+            <div class="nav-bar">
+                <a href="#" class="nav-bar-item">Элемент</a>
+                <a href="#" class="nav-bar-item">Элемент</a>
+                <a href="#" class="nav-bar-item">Элемент</a>
+                <a href="#" class="nav-bar-item">Элемент</a>
+                <a href="#" class="nav-bar-item autorize">Вход</a>
+            </div>
+
+        </div>
+
+        <div class="header-content">
+            <h1 class="header-content-title">Заголовок блока</h1>
+            <p class="header-content-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla facili
+                s ex eum id incidunt ad. Praesentiu
+                m cum consequatur atque est dolor reiciendis veritatis aspernatur
+                aliquam pariatur quasi, accusantium impedit repudiandae!
+            </p>
+            <input type="button" value="Начать обучение!">
+        </div>
+
+    </section>
+
+</body>
+</html>
