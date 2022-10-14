0e1006e14b698157f3a18d1774bde514c91fd57b
Mashero <dima.vuynov@yandex.ru>
2022-10-14 14:40:09 +0300

    Закончен хеддер, добавлены блоки прогресс бара и описания

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.css">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <title>Главная</title>
@@ -39,6 +40,52 @@
             <input class="header-btn" type="button" value="Начать обучение">
         </div>
 
+    </section>
+
+    <section class="about-programm">
+        <div class="container">
+            <h1 class="programm-title">
+                О нашей программе
+            </h1>
+            <p class="programm-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, saepe aliquam architecto quisquam quod neque molestias omnis veritatis ut nesciunt numquam, ducimus atque odit quas obcaecati voluptatibus delectus officia iste.
+            </p>
+        </div>
+    </section>
+
+    <section class="steps-programm">
+        <div class="container">
+            <div class="progress-bar">
+                <div class="progress-bar-item active-progress-bar">
+                    <p class="progress-bar-item-title">1 этап</p>
+                    <i class="fa fa-circle"></i>
+                </div>
+                <div class="progress-bar-item">
+                    <p class="progress-bar-item-title">2 этап</p>
+                    <i class="fa fa-circle"></i>
+                </div>
+                <div class="progress-bar-item">
+                    <p class="progress-bar-item-title">3 этап</p>
+                    <i class="fa fa-circle"></i>
+                </div>
+                <div class="progress-bar-item">
+                    <p class="progress-bar-item-title">4 этап</p>
+                    <i class="fa fa-circle"></i>
+                </div>
+                <div class="progress-bar-item">
+                    <p class="progress-bar-item-title">5 этап</p>
+                    <i class="fa fa-circle"></i>
+                </div>
+            </div>
+            <div class="step-programm-content">
+                <div class="step-programm-item">
+                    <p class="step-programm-item-title">Название этапа</p>
+                    <p class="step-programm-item-text">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro ducimus, vero atque est mollitia veritatis placeat natus debitis harum velit voluptates, nesciunt explicabo, tempora temporibus aut. Similique consectetur placeat explicabo.
+                    </p>
+                </div>
+            </div>
+        </div>
     </section>
 
 </body>
